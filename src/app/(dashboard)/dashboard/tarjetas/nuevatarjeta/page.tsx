53a2cd372690e1ad41f035c9c125e8684d0d78fd
tonga2402 <gbalmaceda@live.com>
2024-08-09 10:35:39 -0300

    FormCard iniciado

--- src/app/(dashboard)/dashboard/tarjetas/nuevatarjeta/page.tsx
+++ src/app/(dashboard)/dashboard/tarjetas/nuevatarjeta/page.tsx
@@ -1,27 +1,93 @@
 "use client";
 import "react-credit-cards-2/dist/es/styles-compiled.css";
 import Cards from "react-credit-cards-2";
+import { FormProvider, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import cardForm from "@/app/schemes/cardForm.scheme";
+import React, { useState } from "react";
 
-const nuevaTarjeta = () => {
+type CardData = {
+  cod: number;
+  expiration_date: string;
+  first_last_name: string;
+  number_id: number;
+};
+
+const NuevaTarjeta = () => {
+  const [cardData, setCardData] = useState({
+    number: "",
+    name: "",
+    expiry: "",
+    cvc: "",
+    focus: "",
+  });
+
+  const { number, name, expiry, cvc } = cardData;
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCardData({
+      ...cardData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  
+  const handleInputFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    setCardData({
+      ...cardData,
+      focus:e.target.name
+    })
+  }
+
+  const onSubmit = ( data:CardData) => {
+  }
+
   return (
     <div className="container_newCard">
-      <Cards cvc={""} expiry={""} name={""} number={""} />
+      <Cards cvc={cvc} expiry={expiry} name={name} number={number} focused={cardData.focus as any} />
 
-      <form action="">
-        <div>
-          <div>
-            <input type="text" name="number" id="number" placeholder="Numero de la tarjeta*"/>
-            <input type="text" name="name" id="name" placeholder="Nombre y apellido*" />
+      <form onSubmit={()=> onSubmit}>
+        <div className="form_newCard">
+          <div className="div_newCard">
+            <input
+              type="text"
+              name="number"
+              id="number"
+              placeholder="Numero de la tarjeta*"
+              onChange={handleInputChange}
+              onFocus={handleInputFocus}
+            />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Nombre y apellido*"
+              onChange={handleInputChange}
+              onFocus={handleInputFocus}
+            />
           </div>
-          <div>
-            <input type="text" name="expiry" id="expiry" placeholder="Fecha de vencimiento*"/>
-            <input type="text" name="cvc" id="cvc" placeholder="Codigo de seguridad*"/>
+          <div className="div_newCard">
+            <input
+              type="text"
+              name="expiry"
+              id="expiry"
+              placeholder="Fecha de vencimiento*"
+              onChange={handleInputChange}
+              onFocus={handleInputFocus}
+            />
+            <input
+              type="text"
+              name="cvc"
+              id="cvc"
+              placeholder="Codigo de seguridad*"
+              onChange={handleInputChange}
+              onFocus={handleInputFocus}
+            />
+            <button>Continuar</button>
           </div>
-          <button>Continuar</button>
         </div>
       </form>
     </div>
   );
 };
 
-export default nuevaTarjeta;
+export default NuevaTarjeta;
