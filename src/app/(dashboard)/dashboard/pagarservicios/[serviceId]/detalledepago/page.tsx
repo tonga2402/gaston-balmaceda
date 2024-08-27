83477577a77baf12f679264516d72bb74c9c8888
tonga2402 <gbalmaceda@live.com>
2024-08-27 10:13:31 -0300

    detalle de pago avanzado

--- src/app/(dashboard)/dashboard/pagarservicios/[serviceId]/detalledepago/page.tsx
+++ src/app/(dashboard)/dashboard/pagarservicios/[serviceId]/detalledepago/page.tsx
@@ -1,21 +1,51 @@
-import Link from 'next/link'
-import React from 'react'
+import SelectCardOptions from "@/app/components/dashboard/cargardinero/SelectCardOptions";
+import Link from "next/link";
+import React from "react";
 
-export default async function PaiService ({}){
+type CardServiceData = {
+  id: number;
+  name: "string";
+  date: "string";
+  invoice_value: number;
+};
 
-    return (
-        <section className="container_section">
-            
-        <div className="section_link">
+export default async function PaiService({
+  params,
+}: {
+  params: { serviceId: number };
+}) {
+
+  const res = await fetch(
+    `https://digitalmoney.digitalhouse.com/service/${params.serviceId}`,
+    {
+      method: "GET",
+    }
+  );
+  const data: CardServiceData = await res.json();
+
+  return (
+    <div className="container_initialPage">
+      <div className="container_pai_service">
+        <div className="div_pai_service">
+          <h2>{data.name}</h2>
           <Link className="link_dashboard" href={"/dashboard/tarjetas"}>
-            Ver tarjetas
+            Ver detalles del pago
           </Link>
-
         </div>
-        <h4>Dinero disponible</h4>
-        <h2>$ {''}</h2>
-      </section>
-    )
+        <hr />
+        <div className="div_pai_service">
+          <h3>Total a pagar</h3>
+          <h3>${data.invoice_value}</h3>
+        </div>
+      </div>
+      <SelectCardOptions />
+      <div style={{ marginTop: "25px" }}>
+        <div className="enter_amount_link">
+          <Link href={""} className="select_card_button">
+            Pagar
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
